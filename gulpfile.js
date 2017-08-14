dd21d3f43c988bf94a8d85affe9a7e708891c470
a42phantom <phantomkemerovo@mail.ru>
2017-08-14 13:31:20 +0300

    начал верстать шапку

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+// Include gulp
+var gulp = require("gulp");
+
+// Include Our Plugins
+//var jshint = require("gulp-jshint");
+var sass = require("gulp-sass");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+var cssnano = require("gulp-cssnano");
+var autoprefixer = require('gulp-autoprefixer');
+var pug = require('gulp-pug');
+
+gulp.task("sass", function() {
+  return gulp.src("src/scss/**/*.scss")
+  .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+  }))
+  .pipe(sass())
+  /*.pipe(cssnano())
+  .pipe(rename({ suffix: '.min' }))*/
+  .pipe(gulp.dest("dist/assets/css/"));
+});
+
+gulp.task("scripts", function() {
+  return gulp.src("src/js/*.js")
+  /*.pipe(uglify())*/
+  .pipe(gulp.dest("dist/assets/js/"));
+});
+
+gulp.task('pug', function() { // buildHTML
+  return gulp.src('src/templates/*.pug')
+  .pipe(pug({
+      locals: '-P',
+      pretty: true
+    })
+  )
+  .pipe(gulp.dest("dist/"));
+});
+
+// Watch Files For Changes
+gulp.task("watch", function() {
+  gulp.watch("src/js/**/*.js", ["scripts"]);
+  gulp.watch("src/scss/**/*.scss", ["sass"]);
+  gulp.watch("src/templates/**/*.pug", ["pug"]);
+});
+
+// Default Task
+gulp.task("default", ["sass", "scripts", "pug", "watch"]);
---
+++ src/js/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+	console.log('script run...');
+	console.log('test');
+	console.log('script end.');
+});
