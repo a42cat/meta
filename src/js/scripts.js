ffa21231743cfe20ca7c51e729a27d99c4ff8c96
a42phantom <phantomkemerovo@mail.ru>
2017-08-30 14:36:36 +0300

    закончены шаблоны: услуги - категории услуги - внутренняя новости - список новости - внутренняя цены

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -17,44 +17,66 @@ function initHeaderMenu() {
 		$(this).next().toggleClass('show');
 	});
 }
-function initAdvantagesSlider() {
-	var advantagesSlider = $('.advantagesSlider');
-	function checkMainSlide(e) {
-		if (typeof e != 'undefined') {
-			$('.advantagesBlock #currentItem').text(e.item.index + 1);
-		}
-		var slideElemCount = advantagesSlider[0].children[0].children[0].childElementCount;
-		$('.advantagesBlock #maxItem').text(slideElemCount);
-		advantagesSlider.find('.owl-item').removeClass('big');
-		advantagesSlider.find('.active:eq(1)').addClass('big');
-	}
-	advantagesSlider.owlCarousel({
+function initNewsInnerPagenSlider() {
+	var newsInnerPagenSlider = $('.newsInnerPagenSlider');
+	newsInnerPagenSlider.owlCarousel({
 		nav: true,
 		navText: ['<img src="assets/images/slideLeft.png">','<img src="assets/images/slideRight.png">'],
-		margin: 10,
+		margin: 20,
+		dots:false,
+		loop:true,
 		responsiveClass:true,
 		responsive : {
 			0 : {
 				items:1
 			},
 			768 : {
-				items:3
-			},
-			992 : {
-				items:3
-			},
-			1200 : {
-				items:3
-			},
-			1900 : {
-				items:3
+				items:2
 			}
 		}
 	});
-	checkMainSlide();
-	advantagesSlider.on('translated.owl.carousel', function(e) {
-		checkMainSlide(e);
-	});
+}
+function initAdvantagesSlider() {
+	var advantagesSlider = $('.advantagesSlider');
+	if (typeof advantagesSlider[0] != 'undefined') {
+		function checkMainSlide(e) {
+			if (typeof e != 'undefined') {
+				$('.advantagesBlock #currentItem').text(e.item.index + 1);
+			}
+			var slideElemCount = advantagesSlider[0].children[0].children[0].childElementCount;
+			$('.advantagesBlock #maxItem').text(slideElemCount);
+			advantagesSlider.find('.owl-item').removeClass('big');
+			advantagesSlider.find('.active:eq(1)').addClass('big');
+		}
+		advantagesSlider.owlCarousel({
+			nav: true,
+			navText: ['<img src="assets/images/slideLeft.png">','<img src="assets/images/slideRight.png">'],
+			margin: 10,
+			loop:true,
+			responsiveClass:true,
+			responsive : {
+				0 : {
+					items:1
+				},
+				768 : {
+					items:3
+				},
+				992 : {
+					items:3
+				},
+				1200 : {
+					items:3
+				},
+				1900 : {
+					items:3
+				}
+			}
+		});
+		checkMainSlide();
+		advantagesSlider.on('translated.owl.carousel', function(e) {
+			checkMainSlide(e);
+		});
+	}
 }
 function initExpertsSlider() {
 	var expertsSlider = $('.expertsSlider');
@@ -110,43 +132,44 @@ function initServicesSliders() {
 }
 function initReviewsSlider() {
 	var reviewsSlider = $('.reviewsSlider');
-	var reviewsSliderDescription = $('.reviewsSliderDescription');
-	function checkMainSlide(e) {
-		var slideElemCount = reviewsSlider[0].children[0].children[0].childElementCount;
-		reviewsSlider.find('.owl-item').removeClass('current');
-		reviewsSlider.find('.active:eq(1)').addClass('current');
-	}
-	reviewsSlider.owlCarousel({
-		dots: false,
-		nav:true,
-		navText: ['<img src="assets/images/slideLeft.png">','<img src="assets/images/slideRight.png">'],
-		loop:true,
-		margin:25,
-		/*autoWidth:true,*/
-		items:4,
-		responsive: {
-			0: {
-				items:1
-			},
-			768: {
-				items:4
-			}
+	if (typeof reviewsSlider[0] != 'undefined') {
+		var reviewsSliderDescription = $('.reviewsSliderDescription');
+		function checkMainSlide(e) {
+			var slideElemCount = reviewsSlider[0].children[0].children[0].childElementCount;
+			reviewsSlider.find('.owl-item').removeClass('current');
+			reviewsSlider.find('.active:eq(1)').addClass('current');
 		}
-	});
-	checkMainSlide();
-	reviewsSlider.on('translated.owl.carousel', function(e) {
-		checkMainSlide(e)
-		reviewsSliderDescription.trigger('to.owl.carousel', e.item.index);
-	});
-	reviewsSliderDescription.owlCarousel({
-		dots: false,
-		nav:false,
-		loop:true,
-		margin:25,
-		items:1,
-		startPosition:4,
-		mouseDrag:false,
-		touchDrag:false
+		reviewsSlider.owlCarousel({
+			dots: false,
+			nav:true,
+			navText: ['<img src="assets/images/slideLeft.png">','<img src="assets/images/slideRight.png">'],
+			loop:true,
+			margin:25,
+			/*autoWidth:true,*/
+			items:4,
+			responsive: {
+				0: {
+					items:1
+				},
+				768: {
+					items:4
+				}
+			}
+		});
+		checkMainSlide();
+		reviewsSlider.on('translated.owl.carousel', function(e) {
+			checkMainSlide(e)
+			reviewsSliderDescription.trigger('to.owl.carousel', e.item.index);
+		});
+		reviewsSliderDescription.owlCarousel({
+			dots: false,
+			nav:false,
+			loop:true,
+			margin:25,
+			items:1,
+			startPosition:4,
+			mouseDrag:false,
+			touchDrag:false
 		/*responsive: {
 			0: {
 				startPosition:1
@@ -156,37 +179,40 @@ function initReviewsSlider() {
 			}
 		}*/
 	});
-	reviewsSliderDescription.on('translated.owl.carousel', function(e) {
-		console.log(e.item.index);
-	});
+		reviewsSliderDescription.on('translated.owl.carousel', function(e) {
+			console.log(e.item.index);
+		});
+	}
 }
 function initPriceSlider() {
 	var priceSlide = $('.priceSlide');
-	function checkMainSlide(e) {
-		var slideElemCount = priceSlide[0].children[0].children[0].childElementCount;
-		priceSlide.find('.owl-item').removeClass('current');
-		priceSlide.find('.active:eq(1)').addClass('current');
-	}
-	priceSlide.owlCarousel({
-		nav:true,
-		dots: true,
-		items:3,
-		loop:true,
-		margin:50,
-		navText: ['<img src="assets/images/slideLeft.png">','<img src="assets/images/slideRight.png">'],
-		responsive: {
-			0: {
-				items:1
-			},
-			768: {
-				items:3
-			}
+	if (typeof priceSlide[0] != 'undefined') {
+		function checkMainSlide(e) {
+			var slideElemCount = priceSlide[0].children[0].children[0].childElementCount;
+			priceSlide.find('.owl-item').removeClass('current');
+			priceSlide.find('.active:eq(1)').addClass('current');
 		}
-	});
-	checkMainSlide();
-	priceSlide.on('translated.owl.carousel', function(e) {
-		checkMainSlide(e)
-	});
+		priceSlide.owlCarousel({
+			nav:true,
+			dots: true,
+			items:3,
+			loop:true,
+			margin:50,
+			navText: ['<img src="assets/images/slideLeft.png">','<img src="assets/images/slideRight.png">'],
+			responsive: {
+				0: {
+					items:1
+				},
+				768: {
+					items:3
+				}
+			}
+		});
+		checkMainSlide();
+		priceSlide.on('translated.owl.carousel', function(e) {
+			checkMainSlide(e)
+		});
+	}
 }
 $(document).ready(function () {
 	console.log('script run...');
@@ -197,6 +223,7 @@ $(document).ready(function () {
 	initServicesSliders();
 	initReviewsSlider();
 	initPriceSlider();
+	initNewsInnerPagenSlider();
 	console.log('script end.');
 });
 
